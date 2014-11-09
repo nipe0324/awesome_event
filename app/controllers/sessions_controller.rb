126c3453fde8d3e40f6430b6fc068476becd5b12
nipe0324 <nipe0324@gmail.com>
2014-11-09 22:47:32 +0900

    Add log-out logic

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,4 +5,10 @@ class SessionsController < ApplicationController
 
     redirect_to root_path, notice: 'ログインしました'
   end
+
+  def destroy
+    reset_session
+
+    redirect_to root_path, notice: 'ログアウトしました'
+  end
 end
