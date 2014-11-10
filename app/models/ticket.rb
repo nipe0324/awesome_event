a45362235a80e19459d95f4de1a240c91d42b980
nipe0324 <nipe0324@gmail.com>
2014-11-10 22:10:46 +0900

    Add create ticket modal

--- app/models/ticket.rb
+++ app/models/ticket.rb
@@ -2,5 +2,5 @@ class Ticket < ActiveRecord::Base
   belongs_to :user
   belongs_to :event
 
-  validates :comment, length: { miximum: 30 }, allow_blank: true
+  validates :comment, length: { maximum: 30 }, allow_blank: true
 end
