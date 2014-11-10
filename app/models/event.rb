7a28839a31ed43ff082aeddce3ff0f7f83fa9e1c
nipe0324 <nipe0324@gmail.com>
2014-11-10 22:34:05 +0900

    Delete ticket

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,5 +1,5 @@
 class Event < ActiveRecord::Base
-  has_many :tickets
+  has_many :tickets, dependent: :destroy
   belongs_to :owner, class_name: 'User'
 
   validates :name,       presence: true, length: { maximum: 50 }
